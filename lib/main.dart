99c6aa219e107a7e0a0f305b209efbe3cfa0731d
emmanuel <akolly@gmail.com>
2023-12-24 09:39:58 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
